a12948f38dfc642b296da7f4bee7f435bf2dab50
Aftab3008 <shaikaftab005@mail.com>
2024-05-18 21:07:17 +0530

    Changes in auth

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,23 +1,23 @@
 import { useUserContext } from "@/context/AuthContext";
-import { Outlet, Navigate } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 export function AuthLayout() {
   const { isAuthenticated } = useUserContext();
+  const navigate = useNavigate();
+  if (isAuthenticated) {
+    navigate("/");
+  }
   return (
     <>
-      {isAuthenticated ? (
-        <Navigate to="/" />
-      ) : (
-        <>
-          <section className="flex flex-1 justify-center items-center flex-col py-10">
-            <Outlet />
-          </section>
-          <img
-            src="/assets/images/side-img.svg"
-            alt="logo"
-            className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat"
-          />
-        </>
-      )}
+      <>
+        <section className="flex flex-1 justify-center items-center flex-col py-10">
+          <Outlet />
+        </section>
+        <img
+          src="/assets/images/side-img.svg"
+          alt="logo"
+          className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat"
+        />
+      </>
     </>
   );
 }
