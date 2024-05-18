786e424e72803afc631e91cf208f47ac5755d6e6
Aftab3008 <shaikaftab005@mail.com>
2024-05-18 19:50:11 +0530

    Added vercel speed insights

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import { BrowserRouter } from 'react-router-dom'
-import AuthProvider from './context/AuthContext.tsx'
-import QueryProvider from './lib/react-query/QueryProvider.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { BrowserRouter } from "react-router-dom";
+import AuthProvider from "./context/AuthContext.tsx";
+import QueryProvider from "./lib/react-query/QueryProvider.tsx";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-    <QueryProvider>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
-    </QueryProvider>
+      <QueryProvider>
+        <AuthProvider>
+          <SpeedInsights />
+          <App />
+        </AuthProvider>
+      </QueryProvider>
     </BrowserRouter>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
