170928ae3a393b099d08c8577191a1df2757c56f
Aftab3008 <shaikaftab005@mail.com>
2024-05-20 16:51:17 +0530

    Logout bug fix

--- src/components/shared/Topbar.tsx
+++ src/components/shared/Topbar.tsx
@@ -1,46 +1,56 @@
-import { Link,useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Button } from "../ui/button";
 import { useSignOutAccount } from "@/lib/react-query/queriesAndMutations";
 import { useEffect } from "react";
-import { useUserContext } from "@/context/AuthContext";
-
+import { INITIAL_USER, useUserContext } from "@/context/AuthContext";
 
 export default function Topbar() {
-  const {mutate:signout,isSuccess} = useSignOutAccount();
-  const {user}=useUserContext();
-    const navigate = useNavigate();
-    useEffect(() => {
-        if(isSuccess){
-            navigate(0)
-        }
-    },[isSuccess,navigate])
+  const { mutate: signout, isSuccess } = useSignOutAccount();
+  const { user, setIsAuthenticated, setUser } = useUserContext();
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (isSuccess) {
+      navigate(0);
+    }
+  }, [isSuccess, navigate]);
+
+  const handleSignOut = async (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    e.preventDefault();
+    signout();
+    setIsAuthenticated(false);
+    setUser(INITIAL_USER);
+    navigate("/sign-in");
+  };
   return (
     <section className="topbar">
       <div className="flex-between py-4 px-5">
         <Link to="/" className="flex gap-3 items-center">
-            <img 
+          <img
             src="/assets/images/logo.svg"
             alt="logo"
             height={325}
             width={130}
-            />
+          />
         </Link>
         <div className="flex gap-4">
-            <Button variant="ghost" className="shad-button_ghost" onClick={()=>signout}>
-                <img
-                src="/assets/icons/logout.svg"
-                alt="logout"
-                />
-            </Button>
-            <Link to={`/profile/${user.id}`} className="flex-center gap-3">
-                <img
-                src={user.imageUrl || "/assets/images/profile-placeholder.svg"}
-                alt="profile"
-                className="rounded-full h-8 w-8"
-                />
-            </Link>
+          <Button
+            variant="ghost"
+            className="shad-button_ghost"
+            onClick={(e) => handleSignOut(e)}
+          >
+            <img src="/assets/icons/logout.svg" alt="logout" />
+          </Button>
+          <Link to={`/profile/${user.id}`} className="flex-center gap-3">
+            <img
+              src={user.imageUrl || "/assets/images/profile-placeholder.svg"}
+              alt="profile"
+              className="rounded-full h-8 w-8"
+            />
+          </Link>
         </div>
       </div>
     </section>
-  )
+  );
 }
